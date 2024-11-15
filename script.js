27d432f204ee3126c6666ade883f848df014e488
Daniel <danielburgoa50@gmail.com>
2024-11-14 23:01:24 -0500

    project done!

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 "use strict";
+
+const btn = document.querySelector("button");
+const img = document.querySelector("img");
+
+const getDog = () => {
+  fetch("https://dog.ceo/api/breeds/image/random")
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error(`Error getting the image. (${res.status})`);
+      }
+
+      return res.json();
+    })
+    .then((data) => (img.src = data.message))
+    .catch((err) => console.log(`Something wen't wrong (${err.message}).`));
+};
+
+btn.addEventListener("click", getDog);
